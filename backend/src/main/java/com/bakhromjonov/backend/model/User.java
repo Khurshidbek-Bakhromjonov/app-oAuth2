2dfc07baf15524aecc53fb3d62dd173a1f67926d
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-15 03:05:53 +0500

    user

--- backend/src/main/java/com/bakhromjonov/backend/model/User.java
+++ backend/src/main/java/com/bakhromjonov/backend/model/User.java
@@ -1,4 +1,41 @@
 package com.bakhromjonov.backend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Entity
+@Table(name = "users", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "email")
+})
+@Getter
+@Setter
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Email
+    @Column(nullable = false)
+    private String email;
+
+    private String imageUrl;
+
+    @JsonIgnore
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+
+    private String providerId;
+
 }
