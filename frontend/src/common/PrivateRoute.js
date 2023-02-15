aeb7f0189e5cbbc7985fa578ca057d349d2f9d21
Khurshidbek Bakhromjonov <khurshidbekjava@gmail.com>
2023-02-15 16:57:05 +0500

    frontend done

--- frontend/src/common/PrivateRoute.js
+++ frontend/src/common/PrivateRoute.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React from 'react';
 import {
     Route,
     Redirect
 } from "react-router-dom";
+
 
 const PrivateRoute = ({component: Component, authenticated, ...rest}) => (
     <Route
